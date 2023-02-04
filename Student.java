4ed4ef57410b6426c34fa80835dd3108be3b9b93
JHON <jhon@gmail.com>
2023-02-04 11:04:59 +0530

    Feature 1 is completed

--- Student.java
+++ Student.java
@@ -5,6 +5,7 @@ class Student{
           System.out.println("Welcome to GIT");
 	  System.out.println("Feature 1 in progress");
 	  System.out.println("Feature 1 in middle");
+	  System.out.println("Feature 1 is completed");
  
  }
 
